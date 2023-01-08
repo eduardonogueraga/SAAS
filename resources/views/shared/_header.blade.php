248b7b5f7ffbb410cb65432d8c1089a6b2fc4b5c
eduardo.nogueraga <eduardo.nogueraga@gmail.com>
2023-01-08 15:07:38 +0100

    Ajustes header

--- resources/views/shared/_header.blade.php
+++ resources/views/shared/_header.blade.php
@@ -1,12 +1,12 @@
 <header class="text-gray-600 body-font">
     <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <a href="{{route('panel.index')}}"  class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <span class="ml-3 text-xl">SAAS</span>
         </a>
-        <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-            <a href="{{route('panel.index')}}" class="mr-5 hover:text-gray-900">Panel de control</a>
-            <a  href="{{route('data.index')}}" class="mr-5 hover:text-gray-900">Datos</a>
-            <a href="{{route('history.index')}}" class="mr-5 hover:text-gray-900">Plan de instalacion</a>
+        <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center">
+            <a href="{{route('panel.index')}}" class="mr-5 hover:text-gray-900 @if(request()->routeIs('panel.index')) font-bold @endif">Panel de control</a>
+            <a  href="{{route('data.index')}}" class="mr-5 hover:text-gray-900 @if(request()->routeIs('data.index')) font-bold @endif">Datos</a>
+            <a href="{{route('history.index')}}" class="mr-5 hover:text-gray-900 @if(request()->routeIs('history.index')) font-bold @endif">Plan de instalacion</a>
             <a class="mr-5 hover:text-gray-900">Configuración y avisos</a>
         </nav>
         <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Usuarios
